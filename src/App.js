58f7119c90ce024f5f8a5d8162a75facac4d15c5
Reid Taylor <rebridgers@gmail.com>
2022-02-19 10:02:54 -0500

    commit this hoss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          To die? ?, Edit <code>src/App.js</code> and save the toad or PERISH.
+          To die? ?, Edit <code>src/App.js</code> and save the toad or PERISH. AND SO THE WORLD ENDED
         </p>
         <a
           className="App-link"
